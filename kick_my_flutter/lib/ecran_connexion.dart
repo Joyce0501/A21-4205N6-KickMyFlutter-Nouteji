25d161933ee72149a3eba735195f66aafc09427e
Joyce <1983276@cegepmontpetit.ca>
2022-09-27 11:47:12 -0400

    message d'attente pour connexion

--- kick_my_flutter/lib/ecran_connexion.dart
+++ kick_my_flutter/lib/ecran_connexion.dart
@@ -20,15 +20,35 @@ class _EcranConnexionState extends State<EcranConnexion> {
 
   String nomConnexion = "";
   String passwordConnexion = "";
-//  late ProgressDialog pr = ProgressDialog(context,type: ProgressDialogType.Normal, isDismissible: true, showLogs: true);
+
+  // final ProgressDialog pr = ProgressDialog(context);
+  // pr = ProgressDialog(context,type: ProgressDialogType.Normal, isDismissible: true/false, showLogs: true/false);
+
+  showLoaderDialog(BuildContext context){
+    AlertDialog alert=AlertDialog(
+      content: new Row(
+        children: [
+          CircularProgressIndicator(),
+          Container(margin: EdgeInsets.only(left: 7),child:Text("Loading..." )),
+        ],),
+    );
+    showDialog(barrierDismissible: false,
+      context:context,
+      builder:(BuildContext context){
+        return alert;
+      },
+    );
+  }
 
   connexion() async {
     try {
+      showLoaderDialog(context);
       SigninRequest req = SigninRequest();
       req.username = nomConnexion;
       req.password = passwordConnexion;
       var reponse = await signin(req);
       print(reponse);
+      Navigator.pop(context);
       Navigator.push(
         context,
         MaterialPageRoute(
